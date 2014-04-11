46f6c64240175f5b413dbd352789ce4b6e68c3db
Gustav Bylund <maistho@gmail.com>
2014-04-11 16:51:29 +0200

    Uses better map implementation from lab6 now

--- src/userprog/map.c
+++ src/userprog/map.c
@@ -7,77 +7,78 @@
 
 void map_init(struct map* m)
 {
-	list_init(&m->content);
-	m->next_key = 2;
-	return;
+  list_init(&m->content);
+  m->next_key = 2;
+  return;
 }
 
-int map_insert(struct map* m, value_t c)
+key_t map_insert(struct map* m, value_t value)
 {
-	struct association *my_ass = malloc(sizeof(struct association));
-	my_ass->value = c;
-	my_ass->key = m->next_key++;
-	list_push_back(&(m->content), &(my_ass->elem));
-	return my_ass->key;
+  struct association *my_ass = malloc(sizeof(struct association));
+  my_ass->value = value;
+  my_ass->key = m->next_key++;
+
+  list_push_back(&m->content, &my_ass->elem);
+  return my_ass->key;
 }
 
-value_t map_find(struct map* m, key_t i)
+value_t map_find(struct map* m, key_t key)
 {
-	struct list_elem *e;
-	for(e = list_rbegin(&(m->content)); e != list_rend(&(m->content)); e = list_prev(e)){
-		if(e->prev == NULL) break;
-		struct association *f = list_entry(e, struct association, elem);
-		if(f->key == i){
-			return &(f->value);
-		}
-	}
-	return NULL;
+  for(struct list_elem *e = list_begin(&(m->content));
+      e != list_end(&(m->content)); e = list_next(e))
+  {
+    struct association *f = list_entry(e, struct association, elem);
+    if (f->key == key)
+    {
+      return f->value;
+    }
+  }
+  return NULL;
 }
 
-value_t map_remove(struct map* m, key_t i)
+value_t map_remove(struct map* m, key_t key)
 {
-	struct list_elem *e;
-	for(e = list_rbegin(&(m->content)); e != list_rend(&(m->content)); e = list_prev(e)){
-		if(e->prev == NULL) break;
-		struct association *f = list_entry(e, struct association, elem);
-		if(f->key == i){
-			//TODO: copy f->value to temp;
-			free(&(f->value));
-			list_remove(&(f->elem));
-			free(f);
-			return NULL;
-		}
-	}
-	return NULL;
+  for(struct list_elem *e = list_begin(&(m->content));
+      e != list_end(&(m->content)); e = list_next(e))
+  {
+    struct association *f = list_entry(e, struct association, elem);
+    if (f->key == key)
+    {
+      list_remove(e);
+      value_t ret = f->value;
+      free(f);
+      return ret;
+    }
+  }
+  return NULL;
 }
 
-
-/*
-	REMOVE?
-	*/
-void map_for_each(struct map* m, void(*exec)(key_t k, value_t v, int i), int i)
+void map_for_each(struct map* m, void(*exec)(key_t k, value_t v, int i), int aux)
 {
-	struct list_elem *e;
-	for(e = list_rbegin(&(m->content)); e != list_rend(&(m->content)); e = list_prev(e)){
-		if(e->prev == NULL) break;
-		struct association *f = list_entry(e, struct association, elem);
-		exec(f->key, f->value, i);
-	}
-	return;
+  for(struct list_elem *e = list_begin(&(m->content));
+      e != list_end(&(m->content)); e = list_next(e))
+  {
+    struct association *f = list_entry(e, struct association, elem);
+    exec(f->key, f->value, aux);
+  }
+  return;
 }
 
-void map_remove_if(struct map* m, bool(*exec)(key_t, value_t, int), int i)
+void map_remove_if(struct map* m, bool(*exec)(key_t, value_t, int), int aux)
 {
-	struct list_elem *e;
-	for(e = list_rbegin(&(m->content)); e != list_rend(&(m->content));){
-		if(e->prev == NULL) break;
-		struct association *f = list_entry(e, struct association, elem);
-		if(exec(f->key, f->value, i)){
-			e = list_prev(e);
-			list_remove(&(f->elem));
-			free(f);
-		}
-	}
-	return;
+  for(struct list_elem *e = list_begin(&(m->content));
+      e != list_end(&(m->content));)
+  {
+    struct association *f = list_entry(e, struct association, elem);
+    if (exec(f->key, f->value, aux))
+    {
+      e = list_remove(e);
+      free(f);
+    }
+    else
+    {
+      e = list_next(e);
+    }
+  }
+  return;
 }
-
