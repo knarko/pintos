7f3ba268a40631813bd59f10c0cfc9cfdf2b5eed
Gustav Bylund <maistho@gmail.com>
2014-05-15 20:29:16 +0200

    oops

---
+++ src/userprog/verify_adr.c
@@ -0,0 +1,50 @@
+#include "userprog/verify_adr.h"
+#include "userprog/pagedir.h"
+#include "threads/vaddr.h"
+#include "lib/stddef.h"
+
+/* Kontrollera alla adresser från och med start till och inte med
+ * (start+length). */
+bool verify_fix_length(void* start, int length)
+{
+	void* adr;
+	void* end = start+length;
+
+	for (adr = pg_round_down(start); adr < end; adr += PGSIZE)
+	{
+		if(pagedir_get_page(NULL, adr) == NULL)
+			return false;
+	}
+	return true;
+}
+
+/* Kontrollera alla adresser från och med start till och med den
+ * adress som först inehåller ett noll-tecken, `\0'. (C-strängar
+ * lagras på detta sätt.) */
+bool verify_variable_length(char* start)
+{
+	char* adr = start;
+	void* page_adr = pg_round_down(adr);
+
+	if (pagedir_get_page(NULL, page_adr) == NULL)
+		return false;
+
+	if (*adr == '\0')
+		return true;
+
+	page_adr += PGSIZE;
+
+	do {
+		++adr;
+		if (adr == page_adr)
+		{
+			if (pagedir_get_page(NULL, page_adr) == NULL)
+			{
+				return false;
+			}
+			page_adr += PGSIZE;
+		}
+	} while (*adr != '\0');
+
+	return true;
+}
---
+++ src/userprog/verify_adr.h
@@ -0,0 +1,8 @@
+#ifndef VERIFY_ADR_H
+#define VERIFY_ADR_H
+#include "lib/stdbool.h"
+
+bool verify_fix_length(void*, int);
+bool verify_variable_length(char*);
+
+#endif
