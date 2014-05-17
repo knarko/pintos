38d5250a2e347ee75c0c17ec46169b799e2c8661
Gustav Bylund <maistho@gmail.com>
2014-05-17 12:57:37 +0200

    Fixed a typo

--- src/tests/klaar/read-bad-buf.c
+++ src/tests/klaar/read-bad-buf.c
@@ -1,4 +1,4 @@
-/* Passes an semingly valid pointer to the read system call.
+/* Passes an seemingly valid pointer to the read system call.
    The pointer will however span invalid pages.
    The process must be terminated with -1 exit code.
    (klaar@ida)
@@ -11,11 +11,11 @@
 char global; /* allocated with process image */
 
 void
-test_main (void) 
+test_main (void)
 {
   int handle;
   char local; /* allocated on process stack */
-  
+
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
 
   /* buffer will start and end at valid addresses ... */
